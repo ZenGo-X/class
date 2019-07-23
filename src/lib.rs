cae4e6a11910da33d00c9ac829f7591beb682681
HAOYUatHZ <haoyu@protonmail.com>
2019-07-23 08:17:58 +0800

    fix for https://github.com/KZen-networks/class-groups/pull/15#discussion_r305713489 & https://github.com/KZen-networks/class-groups/pull/15#discussion_r305717400

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,7 @@ pub struct ABDeltaTriple {
     pub delta: BigInt,
 }
 
+#[derive(Copy, Clone)]
 pub struct Matrix22 {
     pub a11: BigInt,
     pub a12: BigInt,
@@ -49,10 +50,10 @@ pub struct Matrix22 {
 impl Matrix22 {
     pub fn mul(&self, another: Self) -> Self {
         Matrix22 {
-            a11: self.a11 * &another.a11 + self.a12 * &another.a21,
-            a12: self.a11 * &another.a12 + self.a12 * &another.a22,
-            a21: self.a21 * &another.a11 + self.a22 * &another.a21,
-            a22: self.a21 * &another.a12 + self.a22 * &another.a22,
+            a11: self.a11 * another.a11 + self.a12 * another.a21,
+            a12: self.a11 * another.a12 + self.a12 * another.a22,
+            a21: self.a21 * another.a11 + self.a22 * another.a21,
+            a22: self.a21 * another.a12 + self.a22 * another.a22,
         }
     }
 }
