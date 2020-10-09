e4283c9f28beee37d9b6437a71913f7e9f8246f7
HAOYUatHZ <haoyu@protonmail.com>
2020-10-09 16:05:45 +0800

    bump version number to 0.4.18

--- benches/vdf/class_group.rs
+++ benches/vdf/class_group.rs
@@ -2,23 +2,23 @@
 extern crate criterion;
 
 use class_group::primitives::vdf::VDF;
+use class_group::ABDeltaTriple;
 use criterion::Criterion;
 use curv::BigInt;
-use class_group::ABDeltaTriple;
 
 fn benches_class(c: &mut Criterion) {
-    let bench_eval = |c: &mut Criterion, difficulty: &BigInt, a_b_delta: &ABDeltaTriple, seed: &BigInt| {
-        c.bench_function(&format!("eval with difficulty {}", difficulty), move |b| {
-            b.iter(|| VDF::eval(&a_b_delta, &seed, &difficulty))
-        });
-    };
-    let bench_verify =
-        |c: &mut Criterion, difficulty: &BigInt, vdf_out_proof: &VDF| {
-            c.bench_function(
-                &format!("verify with difficulty {}", difficulty),
-                move |b| b.iter(|| vdf_out_proof.verify()),
-            );
+    let bench_eval =
+        |c: &mut Criterion, difficulty: &BigInt, a_b_delta: &ABDeltaTriple, seed: &BigInt| {
+            c.bench_function(&format!("eval with difficulty {}", difficulty), move |b| {
+                b.iter(|| VDF::eval(&a_b_delta, &seed, &difficulty))
+            });
         };
+    let bench_verify = |c: &mut Criterion, difficulty: &BigInt, vdf_out_proof: &VDF| {
+        c.bench_function(
+            &format!("verify with difficulty {}", difficulty),
+            move |b| b.iter(|| vdf_out_proof.verify()),
+        );
+    };
 
     let sec = 1600;
     const TEST_HASH: &str = "1eeb30c7163271850b6d018e8282093ac6755a771da6267edf6c9b4fce9242ba";
@@ -26,7 +26,7 @@ fn benches_class(c: &mut Criterion) {
     let a_b_delta = VDF::setup(sec, &seed);
 
     // change below to `for &i in &[1_000, 2_000, 5_000, 10_000, 100_000, 1_000_000] {` if needed to expand test cases,
-    // may also need to increase pari_init size (first parameter in `pari_init`) in src/primitives/vdf.rs 
+    // may also need to increase pari_init size (first parameter in `pari_init`) in src/primitives/vdf.rs
     for &i in &[1_0] {
         // precompute for verification
         let t = BigInt::from(i);
